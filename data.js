8a51d803b4dbcd5ad1a858d9059d5a6392012f55
Turupawn <ahmed.hn.43@gmail.com>
2016-08-01 17:58:08 -0600

    first commit all links so far

---
+++ data.js
@@ -0,0 +1,84 @@
+var data = [
+
+{
+  name:"Facebook fan page",description:"Funnel",link:"https://www.facebook.com/flatshotgame"
+},
+{
+  name:"Blog",description:"Flatshot's blog",link:"http://flatshot.rosalilastudio.com/"
+},
+{
+  name:"Youtube",description:"Flatshot's youtube chanel",link:"https://www.youtube.com/channel/UC3l-BSOiy8IaHQo6tFca0cQ"
+},
+{
+  name:"System 11",description:"Forum where the sickest shmup players hang out",link:"http://shmups.system11.org/viewtopic.php?f=9&t=57896"
+},
+{
+  name:"IndieDB",description:"A website to showcast indie games",link:"http://www.indiedb.com/games/flatshot"
+},
+{
+  name:"Shmups",description:"Shmups facebook group",link:"https://www.facebook.com/groups/shmups/permalink/10154160124670820/"
+},
+{
+  name:"Comunidad de desarrolladores de videojuegos",description:"Latin american indie game dev group",link:"https://www.facebook.com/groups/comunidad.duval/permalink/1218288131539083/"
+},
+{
+  name:"Indie game developers",description:"Worldwide indie game dev group",link:"https://www.facebook.com/groups/IndieGameDevs/permalink/10153598529391573/"
+},
+{
+  name:"Game Jolt",description:"Worldwide indie game dev group",link:"http://gamejolt.com/games/flatshot/166778"
+},
+{
+  name:"Greenlight",description:"Campaign needed to enter in Steam",link:"http://steamcommunity.com/sharedfiles/filedetails/?id=597403824"
+},
+{
+  name:"How to greenlight first step",description:"Greenlight tutorial",link:"https://playerheartstudio.wordpress.com/2015/12/13/como-dar-tu-primer-paso-en-steam-greenlight/"
+},
+{
+  name:"Press kit maker",description:"Press kit template",link:"http://www.vlambeer.com/press/"
+},
+{
+  name:"Rock Paper Shotgun",description:"PC game reviews, sent an email, they haven't answered",link:"https://www.rockpapershotgun.com/"
+},
+{
+  name:"Indie Royale",description:"TODO",link:"http://www.indieroyale.com/"
+},
+{
+  name:"Indie Game Reviewer",description:"Indie game reviews, sent an email, they haven't answered",link:"http://indiegamereviewer.com/contact-indie-game-reviewer/"
+},
+{
+  name:"Indie Gala",description:"TODO",link:"https://www.indiegala.com/"
+},
+{
+  name:"PC Gamer",description:"PC game reviews, sent an email, they haven't answered",link:"http://www.pcgamer.com/"
+},
+{
+  name:"Libre Game Wiki",description:"TODO",link:"https://libregamewiki.org/Main_Page"
+},
+{
+  name:"Free Gamer Blog",description:"TODO",link:"http://freegamer.blogspot.com/search?updated-max=2016-03-17T15:06:00Z&max-results=5"
+},
+{
+  name:"Free GameDev Forum",description:"TODO",link:"http://forum.freegamedev.net/"
+},
+{
+  name:"Free GameDev Wiki",description:"TODO",link:"http://freegamedev.net/wiki/Main_Page"
+},
+{
+  name:"Free GameDev Planet",description:"TODO",link:"http://planet.freegamedev.net/"
+},
+{
+  name:"Open Game Art",description:"TODO",link:"http://opengameart.org/"
+},
+{
+  name:"Open Source Games Subreddit",description:"TODO",link:"https://www.reddit.com/r/opensourcegames"
+},
+
+
+];
+
+/*
+Special keys:
+* name value
+* text x y
+* lat lng
+*/
